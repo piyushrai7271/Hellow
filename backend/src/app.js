9f933534b072912bc645b063eddac708ac42567f
piyush rai <piyushrai7271@gmail.com>
2026-03-21 13:45:16 +0530

    adding rate limiting on api's

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import {globalRateLimiter} from "./middlewares/rateLimiter.middleware.js"
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 const app = express();
 
@@ -12,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// trust proxy (very Importent in production)
+app.set("trust proxy",1); // help to find correct ip important
+app.use(globalRateLimiter);
 
 // import routes..........
 import userRouter from "./routes/auth.routes.js";
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import {authRateLimiter} from "../middlewares/rateLimiter.middleware.js";
 import {
   registerUser,
   loginUser,
@@ -15,13 +16,18 @@ import jwtValidation from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login",authRateLimiter, loginUser);
 router.post("/logout", jwtValidation, logOutUser);
 router.post("/change-password", jwtValidation, changePassword);
 router.post("/refresh-token", refreshAccessToken);
-router.get("/get-user-details", jwtValidation, getCurrentUser);
+router.get("/get-user-details", jwtValidation,authRateLimiter, getCurrentUser);
 router.post("/addAvatar", jwtValidation, upload.single("avatar"), uploadAvatar);
-router.put("/updateAvatar",jwtValidation,upload.single("avatar"),updateAvatar);
+router.put(
+  "/updateAvatar",
+  jwtValidation,
+  upload.single("avatar"),
+  updateAvatar
+);
 router.delete("/deleteAvatar", jwtValidation, deleteAvatar);
 
 export default router;
